82f05045efc79b4ab5aa0c2c524c072700feaa40
juan diego <juadielonlop07gmail.com>
2026-03-20 13:31:27 -0500

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  tmdbApiKey: 'aab8f105b05448e0898dfa478b82bd03',
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  tmdbImgUrl:  'https://image.tmdb.org/t/p/',
+  tmdbLang:    'es-MX',
+  showId:      1398,
+};
