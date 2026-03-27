40b4f7473303dc071f637b1386cc77582f867935
juan diego <juadielonlop07gmail.com>
2026-03-27 15:07:30 -0500

    9

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-
-
-    provideHttpClient(
-      withFetch()
-    ),
-
-  ]
+    provideHttpClient(withFetch()),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
